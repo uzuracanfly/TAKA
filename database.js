8ac816b5d5afbb484dd2d49db338a23badc8901f
uzuracanfly <uzuracanfly@gmail.com>
2020-03-25 11:09:06 +0900

    update commit

--- database.js
+++ database.js
@@ -383,8 +383,6 @@ exports.RunCommit = async function(){
 			if (transactions.length > 0){
 				let transaction = transactions[0];
 
-				transactions.shift();
-
 				if (transaction["function"] == "set"){
 					await save(transaction["args"]["database"],transaction["args"]["table"],transaction["args"]["index"],transaction["args"]["data"]);
 
@@ -424,6 +422,8 @@ exports.RunCommit = async function(){
 					(transaction["response"]).write(JSON.stringify(result));
 					(transaction["response"]).end();
 				}
+
+				transactions.shift();
 			};
 		}catch(e){
 			console.log(e);
